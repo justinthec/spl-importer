731c9f3c2af19f07cb11b3dac42b15d436aa11f3
Justin Chan <justin.the.c@gmail.com>
2015-03-02 22:15:39 -0500

    Added faiure view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
+  def success
     @auth = request.env['omniauth.auth']['credentials']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SplImporter::Application.routes.draw do
   root to: 'sessions#new'
   resources :sessions, only: :index
-  get "/auth/:provider/callback" => 'sessions#create'
+  get "/auth/:provider/callback" => 'sessions#success'
+  get "/auth/failure" => 'sessions#failure'
 end
