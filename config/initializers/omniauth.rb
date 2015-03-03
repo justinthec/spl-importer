5f2615f46f94c43f8e6ed7169cd6908b4b5633e6
Justin Chan <justin.the.c@gmail.com>
2015-03-02 21:56:17 -0500

    stil tring

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, Rails.application.secrets.client_id, Rails.application.secrets.client_secret, {
-  scope: ['https://www.googleapis.com/auth/calendar'],
+  scope: ['email',
+    'https://www.googleapis.com/auth/calendar'],
     access_type: 'online'}
 end
