fc890d43ecbb06f359a81a05fc6ef74cb2e988cf
Justin Chan <justin.the.c@gmail.com>
2015-03-02 21:52:14 -0500

    still fixing gapi

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, Rails.application.secrets.client_id, Rails.application.secrets.client_secret, {
-  scope: ['',
-    'https://www.googleapis.com/auth/calendar'],
+  scope: ['https://www.googleapis.com/auth/calendar'],
     access_type: 'online'}
 end
