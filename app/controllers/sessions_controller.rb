e7799a0253d3e959d262efa05c2e200559a536c0
Justin Chan <justin.the.c@gmail.com>
2015-03-02 23:47:14 -0500

    Trying google cal auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -72,90 +72,90 @@ class SessionsController < ApplicationController
 
 		puts "#{@matches.count} Matches found..."
 
-		# # Initialize the client.
-		# client = Google::APIClient.new(
-		#   :application_name => 'SPL Importer',
-		#   :application_version => '1.0.0'
-		# )
+		# Initialize the client.
+		client = Google::APIClient.new(
+		  :application_name => 'SPL Importer',
+		  :application_version => '1.0.0'
+		)
 
-		# # Initialize Google Calendar API. Note this will make a request to the
-		# # discovery service every time, so be sure to use serialization
-		# # in your production code. Check the samples for more details.
-		# calendar = client.discovered_api('calendar', 'v3')
+		# Initialize Google Calendar API. Note this will make a request to the
+		# discovery service every time, so be sure to use serialization
+		# in your production code. Check the samples for more details.
+		calendar = client.discovered_api('calendar', 'v3')
 
-		# # Load client secrets from your client_secrets.json.
-		# client_secrets = Google::APIClient::ClientSecrets.load
+		# Load client secrets from your client_secrets.json.
+		client_secrets = Google::APIClient::ClientSecrets.load
 
-		# # Run installed application flow. Check the samples for a more
-		# # complete example that saves the credentials between runs.
-		# flow = Google::APIClient::InstalledAppFlow.new(
-		#   :client_id => client_secrets.client_id,
-		#   :client_secret => client_secrets.client_secret,
-		#   :scope => ['https://www.googleapis.com/auth/calendar']
-		# )
-		# client.authorization = flow.authorize
+		# Run installed application flow. Check the samples for a more
+		# complete example that saves the credentials between runs.
+		flow = Google::APIClient::InstalledAppFlow.new(
+		  :client_id => client_secrets.client_id,
+		  :client_secret => client_secrets.client_secret,
+		  :scope => ['https://www.googleapis.com/auth/calendar']
+		)
+		client.authorization = flow.authorize
 
-		# # Get list of calendars
-		# list_of_calendars = client.execute(
-		#   :api_method => calendar.calendar_list.list,
-		# )
+		# Get list of calendars
+		list_of_calendars = client.execute(
+		  :api_method => calendar.calendar_list.list,
+		)
 
-		# puts "Google API authenticated..."
+		puts "Google API authenticated..."
 
-		# # Deletes SPL Calendar if it already exists
-		# list_of_calendars.data.items.each do |calendar_item|
-		#   if (calendar_item.summary == "Starcraft 2 Proleague") then
-		#     puts "SPL Calendar found, deleting..."
-		#     delete_spl = client.execute(
-		#       :api_method => calendar.calendars.delete,
-		#       :parameters => {'calendarId' => calendar_item.id}
-		#     )
-		#     puts "SPL Calendar deleted..."
-		#   end
-		# end
+		# Deletes SPL Calendar if it already exists
+		list_of_calendars.data.items.each do |calendar_item|
+		  if (calendar_item.summary == "Starcraft 2 Proleague") then
+		    puts "SPL Calendar found, deleting..."
+		    delete_spl = client.execute(
+		      :api_method => calendar.calendars.delete,
+		      :parameters => {'calendarId' => calendar_item.id}
+		    )
+		    puts "SPL Calendar deleted..."
+		  end
+		end
 
-		# # Creates new SPL Calendar
-		# spl_calendar = {
-		#   'summary' => "Starcraft 2 Proleague",
-		#   'timeZone' => "Asia/Seoul"
-		# }
+		# Creates new SPL Calendar
+		spl_calendar = {
+		  'summary' => "Starcraft 2 Proleague",
+		  'timeZone' => "Asia/Seoul"
+		}
 
-		# create_spl = client.execute(
-		#   :api_method => calendar.calendars.insert,
-		#   :body => JSON.dump(spl_calendar),
-		#   :headers => {'Content-Type' => 'application/json'}
-		# )
+		create_spl = client.execute(
+		  :api_method => calendar.calendars.insert,
+		  :body => JSON.dump(spl_calendar),
+		  :headers => {'Content-Type' => 'application/json'}
+		)
 
-		# puts "SPL Calendar created..."
+		puts "SPL Calendar created..."
 
-		# # Save SPL Calendar ID for creating events
-		# spl_calendar_id = create_spl.data.id
+		# Save SPL Calendar ID for creating events
+		spl_calendar_id = create_spl.data.id
 
-		# puts "Importing Matches..."
+		puts "Importing Matches..."
 
-		# # Creates Events for each of the Matches
-		# matches.each do |match|
-		#   match_event = {
-		#     'summary' => "#{match.team1} vs #{match.team2}",
-		#     'start' => {
-		#       'dateTime' => DateTime.new(match.startyear, match.startmonth, match.startdate, match.starthour, match.startmin, 0, '+9').to_s,
-		#       'timeZone' => "Asia/Seoul"
-		#     },
-		#     'end' => {
-		#       'dateTime' => DateTime.new(match.startyear, match.startmonth, match.startdate, match.starthour+1, match.startmin, 0, '+9').to_s,
-		#       'timeZone' => "Asia/Seoul"
-		#     }
-		#   }
+		# Creates Events for each of the Matches
+		@matches.each do |match|
+		  match_event = {
+		    'summary' => "#{match.team1} vs #{match.team2}",
+		    'start' => {
+		      'dateTime' => DateTime.new(match.startyear, match.startmonth, match.startdate, match.starthour, match.startmin, 0, '+9').to_s,
+		      'timeZone' => "Asia/Seoul"
+		    },
+		    'end' => {
+		      'dateTime' => DateTime.new(match.startyear, match.startmonth, match.startdate, match.starthour+1, match.startmin, 0, '+9').to_s,
+		      'timeZone' => "Asia/Seoul"
+		    }
+		  }
 
-		#   create_match = client.execute(
-		#     :api_method => calendar.events.insert,
-		#     :parameters => {'calendarId' => spl_calendar_id},
-		#     :body => JSON.dump(match_event),
-		#     :headers => {'Content-Type' => 'application/json'}
-		#   )
+		  create_match = client.execute(
+		    :api_method => calendar.events.insert,
+		    :parameters => {'calendarId' => spl_calendar_id},
+		    :body => JSON.dump(match_event),
+		    :headers => {'Content-Type' => 'application/json'}
+		  )
 
-		#   match.print
-		# end
+		  match.print
+		end
 
 		# Done
 		puts "Match Import Complete!"
