f6dbb12160ee3d187c2ffe5cce6748b11b5bbc56
Justin Chan <justin.the.c@gmail.com>
2015-03-09 00:11:38 -0400

    Made minor performance improvements to the controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -49,8 +49,7 @@ class SessionsController < ApplicationController
 		require 'date'
 
 		# Pages array to store the pages we will pull the matches from
-		pages = ['http://wiki.teamliquid.net/starcraft2/2015_Proleague/Round_1/Round_Robin',
-		         'http://wiki.teamliquid.net/starcraft2/2015_Proleague/Round_2/Round_Robin']
+		pages = ['http://wiki.teamliquid.net/starcraft2/2015_Proleague/Round_2/Round_Robin']
 
 		# Matches array to store the matches we will add to the Calendar
 		@matches = []
@@ -62,17 +61,20 @@ class SessionsController < ApplicationController
 		  # Parse through the HTML for the match info and populate the matches array
 		  doc.css('#mw-content-text div[style="display:inline-block; vertical-align: top; margin: 0 0 0 0;padding-right:2em;"]').each do |match|
 		    if (match.content.strip != "") then
-		      team1 = match.css('table')[0].css('tr td')[0].content.gsub("\302\240", ' ').strip
-		      team2 = match.css('table')[0].css('tr td')[3].content.gsub("\302\240", ' ').strip
-		      time = match.css('table')[1].css('tr th span').select{|link| link['style'] =~ /margin-left:.*40px;.*font-size:.*85%;.*line-height:.*90%;/}[0].text
-          timezone = match.css('table')[1].css('tr th span').select{|link| link['style'] =~ /margin-left:.*40px;.*font-size:.*85%;.*line-height:.*90%;/}[0].css('abbr')[0]['data-tz']
+		    	match_table_row = match.css('table')[0].css('tr td')
+		    	time_container = match.css('table')[1].css('tr th span').select{|link| link['style'] =~ /margin-left:.*40px;.*font-size:.*85%;.*line-height:.*90%;/}[0]	
 
-		      time_array = time.split
-		      year = time_array[2]
-		      month = Date::MONTHNAMES.index(time_array[0])
-		      date = time_array[1].gsub(/,/, '')
-		      hour = time_array[3].slice(0..1)
-		      min = time_array[3].slice(3..4)
+		      team1 = match_table_row[0].content.gsub("\302\240", ' ').strip
+		      team2 = match_table_row[3].content.gsub("\302\240", ' ').strip
+		      time = time_container.text
+          timezone = time_container.css('abbr')[0]['data-tz']
+
+		      time_segments = time.split
+		      year = time_segments[2]
+		      month = Date::MONTHNAMES.index(time_segments[0])
+		      date = time_segments[1].gsub(/,/, '')
+		      hour = time_segments[3].slice(0..1)
+		      min = time_segments[3].slice(3..4)
 
 		      match = Match.new(team1, team2, year, month, date, hour, min, timezone)
 		      if(match.endtime > DateTime.now) then
