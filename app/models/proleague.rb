a4051d0dbe04041cb66fa608068f0285fe8c9075
Justin Chan <justin.the.c@gmail.com>
2015-05-10 22:32:43 -0400

    Fixed Web Scraper and Manually updated the hosted calendar

--- app/models/proleague.rb
+++ app/models/proleague.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 class Proleague
 	# Pages array to store the pages we will pull the matches from
 	@@pages = [
-		'http://wiki.teamliquid.net/starcraft2/2015_Proleague/Round_3',
+		#'http://wiki.teamliquid.net/starcraft2/2015_Proleague/Round_3',
 		'http://wiki.teamliquid.net/starcraft2/2015_Proleague/Round_3/Round_Robin'
 	]
 
@@ -16,13 +16,13 @@ class Proleague
 		  doc = Nokogiri::HTML(open(page))
 
 		  # Parse through the HTML for the match info and populate the matches array
-		  doc.css('#mw-content-text div[style="display:inline-block; vertical-align: top; margin: 0 0 0 0;padding-right:2em;"]').each do |match|
+		  doc.css('div[style="display:inline-block; vertical-align: top; margin: 0 0 0 0;padding-right:2em;"]').each do |match|
 		    if (match.content.strip != "") then
 		    	match_table_row = match.css('table')[0].css('tr td')
 		    	time_container = match.css('table')[1].css('tr th span').select{|link| link['style'] =~ /margin-left:.*40px;.*font-size:.*85%;.*line-height:.*90%;/}[0]	
 
-		      team1 = match_table_row[0].content.gsub("\302\240", ' ').strip
-		      team2 = match_table_row[3].content.gsub("\302\240", ' ').strip
+		      team1 = match_table_row[0].css('span[class="team-template-text"] a').text.gsub("\302\240", ' ').strip
+		      team2 = match_table_row[3].css('span[class="team-template-text"] a').text.gsub("\302\240", ' ').strip
 		      time = time_container.text
           timezone = time_container.css('abbr')[0]['data-tz']
 
